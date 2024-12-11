a34d128a213e2eb08ff4c9d3c789285401a3ad1f
rayhankobir-dev <rayhankobir793@gmail.com>
2024-12-12 01:03:21 +0600

    feat: improved side nav style

--- src/components/layouts/sidenav.tsx
+++ src/components/layouts/sidenav.tsx
@@ -1,60 +1,54 @@
 import { cn } from "@/lib/utils";
-import { PillBottle } from "lucide-react";
-import { NavLink } from "react-router-dom";
-import Doctor from "@/assets/vectors/doctor.svg";
 import { Button } from "../ui/button";
+import { NavLink } from "react-router-dom";
+import Doctor from "@/assets/vectors/doctor.png";
+import Medicine from "@/assets/vectors/medicines.png";
+import DoctorVector from "@/assets/vectors/doctor.svg";
 
 const links = [
   {
     label: "Medicines",
-    icon: PillBottle,
+    icon: Medicine,
     to: "/medicines",
   },
   {
     label: "Surgical",
-    icon: PillBottle,
+    icon: Doctor,
     to: "/category/surgical",
-  },
-  {
-    label: "Diagnostic",
-    icon: PillBottle,
-    to: "/category/diagnostic",
-  },
-  {
-    label: "Health Care",
-    icon: PillBottle,
-    to: "/category/healthcare",
-  },
-  {
-    label: "Other",
-    icon: PillBottle,
-    to: "/category/other",
   },
 ];
 
 function SideNav() {
   return (
-    <aside className="h-[80vh] w-full sticky top-[4.5rem] flex flex-col justify-between mt-5 rounded-2xl bg-gray-100 border border-primary/10 shadow-sm overflow-hidden">
+    <aside className="h-[80vh] w-full sticky top-[4.5rem] flex flex-col justify-between mt-5 rounded-2xl bg-primary-foreground border border-primary/20 shadow-sm overflow-hidden">
       <div className="flex flex-col">
         {links.map((link) => (
           <NavLink
             className={({ isActive }) =>
               cn(
-                "flex items-center py-3 px-3 text-md hover:bg-primary/10 border-b border-primary/10 font-medium tracking-tighter",
-                isActive && "bg-primary/80 text-white hover:bg-primary/90"
+                "flex items-center gap-3 py-2.5 px-3 text-md bg-primary/5 hover:bg-primary/10 border-b border-primary/20 font-medium text-lg",
+                isActive && "bg-primary/30 text-primary hover:bg-primary/30"
               )
             }
             key={link.label}
             to={link.to}
           >
-            <link.icon className="mr-2 h-4 w-4" />
+            <img
+              className="h-10 w-10 rounded-full"
+              src={link.icon}
+              alt={link.label}
+            />
             {link.label}
           </NavLink>
         ))}
       </div>
 
       <div className="flex flex-col items-center justify-center gap-2 bg-primary-foreground p-5">
-        <img className="max-w-[150px] mx-auto" src={Doctor} alt="Vector" />
+        <img
+          className="max-w-[150px] mx-auto"
+          src={DoctorVector}
+          alt="Vector"
+        />
         <h1 className="font-medium text-lg">Free Consultation</h1>
         <p className="font-medium light text-sm text-center opacity-70">
           Take free Consultation with our doctors for free firt time.
